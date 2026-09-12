da1f2cc103f2dd71fa8fc58414160e88bbf27c54
Musab Bin Umair <musabislam456@gmail.com>
2026-09-12 16:55:33 +0500

    fix: correct production domain (mediaconvert.toolbay.site)

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://mediaconvert.io/sitemap.xml',
+    sitemap: 'https://mediaconvert.toolbay.site/sitemap.xml',
   };
 }
